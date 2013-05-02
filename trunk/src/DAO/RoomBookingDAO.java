57d121f41b9d7b4a519c3fb69e2511143d353dae
tomiberes@gmail.com <tomiberes@gmail.com@46dde3af-50d2-f6e7-33fb-27efaeb80fa7>
2013-05-02 19:59:57 +0000

    cleaned up the mess, added RoomDAO and IFRoomDAO since logically findFreeRooms() should be placed in there. We need to have 1 more DAO class for the first use-case.

---
+++ trunk/src/DAO/RoomBookingDAO.java
@@ -0,0 +1,69 @@
+package DAO;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Models.RoomBooking;
+
+public class RoomBookingDAO implements IFRoomBookingDAO {
+
+	public RoomBookingDAO() {
+		// con = DBConnection.getInstance().getDBCon();
+	}
+
+	@Override
+	public RoomBooking getRoomBooking(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<RoomBooking> getAllRoomBookings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(RoomBooking roomBooking) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(RoomBooking roomBooking) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@SuppressWarnings("unused")
+	private RoomBooking singleWhere(String wClause, boolean retrieveAssociation) {
+		// TODO
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private ArrayList<RoomBooking> miscWhere(String wClause,
+			boolean retrieveAssociation) {
+		// TODO
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private RoomBooking buildRoomBooking(ResultSet results) {
+		// TODO
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private String buildQuery(String wClause) {
+		// TODO
+		return "";
+	}
+
+}
---
+++ trunk/src/DAO/RoomDAO.java
@@ -0,0 +1,77 @@
+package DAO;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import Models.Room;
+import Models.RoomType;
+
+public class RoomDAO implements IFRoomDAO {
+
+	public RoomDAO() {
+		// con = DBConnection.getInstance().getDBCon();
+	}
+
+	@Override
+	public Room getRoom(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Room> getAllRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Room> findFreeRooms(String startDate, String endDate,
+			Enum<RoomType> roomType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(Room room) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Room room) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@SuppressWarnings("unused")
+	private Room singleWhere(String wClause, boolean retrieveAssociatio) {
+		// TODO
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private ArrayList<Room> miscWhere(String wClause,
+			boolean retrieveAssociation) {
+		// TODO
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private Room buildRoom(ResultSet results) {
+		// TODO
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private String buildQuery(String wClause) {
+		// TODO
+		return "";
+	}
+
+}
