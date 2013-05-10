b74fbd1fe9600776e348be82e186f4b9f2b95b40
david.vannay@gmail.com <david.vannay@gmail.com@46dde3af-50d2-f6e7-33fb-27efaeb80fa7>
2013-05-10 11:09:52 +0000

    added script for inserting instructor to the database for testing Note, it uses last inserted id, enter it either separately or change the value of the ID.

--- trunk/src/SQL/sampleData.sql
+++ trunk/src/SQL/sampleData.sql
@@ -7,10 +7,14 @@ INSERT INTO Location(ZIP, country, city)
 	VALUES('9000', 'Denmark', 'Aalborg');
 
 INSERT INTO Person(CPR, fname, lname, address, locationZIP, country, email, password, personType)
-	VALUES('123456-7890', 'Some', 'One', 'Street and number 3', '9000', 'Denmark', 'someone@somemail.dk', 'somepass', 'Customer');
+	VALUES('123456-7890', 'Some', 'One', 'Street and number 3', '9000', 'Denmark', 'someone@somemail.dk', 'somepass', 'Customer'),
+	('987654-3210','No','One','Boulevarden 55','9000','Denmark','noone@nomail.dk', 'shitsinked', 'Instructor');
 
 INSERT INTO Customer(customerID, registrationDate, noOfStays)
 	VALUES((SELECT IDENT_CURRENT('Person')), '30-12-2013', 0);
+	
+INSERT INTO Staff(staffID, staffRank, salary, staffType)
+	VALUES((SELECT IDENT_CURRENT('Person')), 15000, Instructor);
 
 INSERT INTO RoomBooking(customerID, dateStart, dateEnd, dateBooked)
 	VALUES((SELECT IDENT_CURRENT('Person')), '13-08-2014', '24-08-2014', '05-06-2014');
