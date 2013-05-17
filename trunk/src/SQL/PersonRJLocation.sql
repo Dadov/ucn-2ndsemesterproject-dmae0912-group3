41b432d61ea68cb5affc45770b49a1d6d65a7712
stoykovnet@gmail.com <stoykovnet@gmail.com@46dde3af-50d2-f6e7-33fb-27efaeb80fa7>
2013-05-17 09:16:59 +0000

    Hey guys, I have made a RIGHT JOIN script, it joins Person and Location tables. I made it, because I need to retrieve the city for a Person. I hope this is an appropriate solution, please check it, and tell me whether it is good or not;
    made to retrieve information about single Person and multiple Persons.
    Also there are some WHERE clauses;
    
    Adiós amigos! :)
    
    //stefo;

---
+++ trunk/src/SQL/PersonRJLocation.sql
@@ -0,0 +1,30 @@
+USE Morocco;
+
+/* These SQL scripts use RIGHT JOIN on Person and Location tables.
+ * The deal is that city value for a Person must be retrieved. There
+ * are four options, in order to provide more flexibility in the
+ * search of Person. They are placed altogther so you can get an
+ * overview easily.
+ * You can try them with no problem, 'cause Tomas Some One was used
+ * as an example.
+ */
+SET DATEFORMAT dmy;
+
+/* Makes a table with a single entry by personID */
+SELECT * FROM Person RIGHT JOIN Location
+ON Person.locationZIP=Location.ZIP
+WHERE personID = 1;
+
+/* Makes a table with a single entry by CPR */
+SELECT * FROM Person RIGHT JOIN Location
+ON Person.locationZIP=Location.ZIP
+WHERE CPR = '123456-7890';
+
+/* Makes a table with a single entry by fname & lname */
+SELECT * FROM Person RIGHT JOIN Location
+ON Person.locationZIP=Location.ZIP
+WHERE fname = 'Some' and lname = 'One';
+
+/* Makes a table with multiple entries */
+SELECT * FROM Person RIGHT JOIN Location
+ON Person.locationZIP=Location.ZIP;
