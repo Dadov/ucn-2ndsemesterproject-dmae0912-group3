9e8e79a1edb48d1202834f80e9f5dcc06812d9c5
tomiberes@gmail.com <tomiberes@gmail.com@46dde3af-50d2-f6e7-33fb-27efaeb80fa7>
2013-05-27 11:30:23 +0000

    small update

--- trunk/src/GUI/CustomersGUI.java
+++ trunk/src/GUI/CustomersGUI.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class CustomersGUI extends JPanel {
@@ -8,8 +10,13 @@ public class CustomersGUI extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JPanel customersWrapper;
 
 	public CustomersGUI() {
+
+		customersWrapper = new JPanel();
+		customersWrapper.setPreferredSize(new Dimension(780, 535));
+		add(customersWrapper);
 		// TODO placeholder should be replaced by SWING class
 	}
 
--- trunk/src/GUI/StaffGUI.java
+++ trunk/src/GUI/StaffGUI.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class StaffGUI extends JPanel {
@@ -8,8 +10,13 @@ public class StaffGUI extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JPanel staffWrapper;
 
 	public StaffGUI() {
+
+		staffWrapper = new JPanel();
+		staffWrapper.setPreferredSize(new Dimension(780, 535));
+		add(staffWrapper);
 		// TODO placeholder should be replaced by SWING class
 	}
 
